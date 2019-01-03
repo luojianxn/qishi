905884e2dafc877433f9387ea114c6dba476b907
121 <123>
2019-01-03 22:58:21 +0800

    w123
    123

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,6 +6,7 @@ Page({
     logs: []
   },
   onLoad: function () {
+    console.log(123);
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
